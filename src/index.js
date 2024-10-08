c7419df7a36492d8464560c0a37d1db60f59e558
Sebastien J <sebastienjerome@gmail.com>
2024-10-08 15:10:54 -0400

    Added main configuration and index file for library calls

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+const sendAs2Message = require('./sendAs2Message');
+const receiveAs2Message = require('./receiveAs2Message');
+
+module.exports = {
+  sendAs2Message,
+  receiveAs2Message
+};
